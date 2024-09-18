cb71bbb7a9eaed39ef0d3a19e449d4787e20d9d5
Sourav <sourav@ndtvs-MacBook-Air.local>
2024-09-18 12:18:18 +0530

    crud nest

--- lesson1/src/users/users.controller.ts
+++ lesson1/src/users/users.controller.ts
@@ -1,10 +1,21 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
-
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
+import { UsersService } from './users.service';
 @Controller('users') // /users
 export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
   @Get() // GET /users or /users?role=value1&age=value2
-  findAll(@Query('role')role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
-    return [];
+  findAll(@Query('role') role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
+    // return [];
+    return this.usersService.findAll(role);
   }
   //   @Get('interns') //GET /interns
   //   findAllInterns() {
@@ -12,18 +23,37 @@ export class UsersController {
   //   }
   @Get(':id') // GET /users/:id
   findOne(@Param('id') id: string) {
-    return { id };
+    // return { id };
+    return this.usersService.findOne(+id);
   }
   @Post() // POST /users
-  create(@Body() user: object) {
-    return user;
+  create(
+    @Body()
+    user: {
+      name: string;
+      email: string;
+      role: 'INTERN' | 'ENGINEER' | 'ADMIN';
+    },
+  ) {
+    // return user;
+    return this.usersService.create(user);
   }
   @Patch(':id') // PATCH /users/:id
-  update(@Param('id') id: string, @Body() userUpdate: object) {
-    return { id, ...userUpdate };
+  update(
+    @Param('id') id: string,
+    @Body()
+    userUpdate: {
+      name?: string;
+      email?: string;
+      role?: 'INTERN' | 'ENGINEER' | 'ADMIN';
+    },
+  ) {
+    // return { id, ...userUpdate };
+    return this.usersService.update(+id, userUpdate);
   }
   @Delete(':id') // DELETE /users/:id
   delete(@Param('id') id: string) {
-    return { id };
+    // return { id };
+    return this.usersService.delete(+id);
   }
 }
