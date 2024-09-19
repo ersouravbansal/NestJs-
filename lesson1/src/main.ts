ced5d825e966c56fa2364fb85bf873cf55d3130d
Sourav <sourav@ndtvs-MacBook-Air.local>
2024-09-19 15:11:23 +0530

    rate limiter, exception filters,logger,cors, globalprefix,

--- lesson1/src/main.ts
+++ lesson1/src/main.ts
@@ -1,8 +1,18 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, HttpAdapterHost } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { AllExceptionFilter } from './all-execptions.filter';
+// import { MyLoggerService } from './my-logger/my-logger.service';
 
 async function bootstrap() {
+  // const app = await NestFactory.create(AppModule, {
+  //   bufferLogs: true,
+  // });
+  // app.useLogger(app.get(MyLoggerService));
   const app = await NestFactory.create(AppModule);
+  const { httpAdapter } = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new AllExceptionFilter(httpAdapter));
+  app.enableCors();
+  app.setGlobalPrefix('api');
   await app.listen(3000);
 }
 bootstrap();
